6596c3fb114d499eb3a3ec1efe2c14f7c42fd65f
code with ayush <marcusayush3@gmail.com>
2022-05-15 15:05:52 +0530

    Toon Tanks Completed

---
+++ Assets/Scripts/TankSelection.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+    public TankSpwaner tankSpwaner;
+    public void GreenTankSelected()
+    {
+        Debug.Log("Green Tank Selected");
+        tankSpwaner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+    }
+
+    public void BlueTankSelected()
+    {
+        tankSpwaner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+    }
+    public void RedTankSelected()
+    {
+    tankSpwaner.CreateTank(TankTypes.RedTank);
+    this.gameObject.SetActive(false);
+    }
+
+}
